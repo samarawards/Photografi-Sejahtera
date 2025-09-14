4e37b37a9e30e9b7e97fab7d5e6938b2c01604e5
samarawards <samarawardasadiya@gmail.com>
2025-09-14 14:06:44 +0800

    buat signin login

---
+++ login/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sign In</title>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" href="/photograf\login\assets\css\style.css">
+    </head>
+    <body>
+        <div>
+            <header>Logo</header>
+            <div class="box">
+                <h1>Log In</h1>
+                <p>Lorem ipsum dolor sit amet adipiscing elit.</p>
+                <form>
+                    <label>Email*</label>
+                    <input type="text" required>
+                    
+                    <div class="forgot-pass">
+                        <label>Password*</label> 
+                        <a href="#">Forgot yout password?</a>
+                    </div>
+                    <input type="email" required>
+                    <button class="btn-black">Sign up</button>
+                    <button class="btn-white"><i class='bx bxl-google'></i> Sign up with Google</button>
+                    <div class="signup-link">
+                        Don't have an account? <a href="http://localhost/photograf/signingpt/">Sign up</a>
+                    </div>
+                </form>
+            </div>
+            <footer>© 2025 NamaKelompok</footer>
+        </div>
+
+        <div style="background-color: grey;">
+            <img src="\photograf\logingpt\assets\image\nct-dream-chenle-hot-sauce-4k-wallpaper-3840x2160-uhdpaper.com-109.0_a.jpg" width="100%" height="100%">
+        </div>
+    </body>
+        </html>
---
+++ signin/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign Up</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="/photograf\signin\assets\css\style.css">
+</head>
+<body>
+  <header>Logo</header>
+    <div class="box">
+        <h1>Sign Up</h1>
+        <p>Lorem ipsum dolor sit amet adipiscing elit.</p>
+        <form>
+        <label>Name*</label>
+        <input type="text" required>
+        
+        <label>Email*</label>
+        <input type="email" required>
+
+        <label>Password*</label>
+        <input type="password" required>
+
+        <button class="btn-black">Sign up</button>
+        <button class="btn-white"><i class='bx bxl-google'></i> Sign up with Google</button>
+        </form>
+        
+        <div class="login-link">
+            Already have an account? <a href="#" tabindex="0" role="link">Log In</a>
+        </div>
+    </div>
+  <footer>© 2025 NamaKelompok</footer>
+</body>
+</html>
